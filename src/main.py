24000c73952382030e29a65065fbd346e44e7acc
Joshua-S-Gilbert <joshuagilbert77@gmail.com>
2024-05-29 12:24:31 +1000

    using static world

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main():
     worldFile = "/home/student/catkin_ws/src/3806Robotics/src/world.txt"
     testFile = "/home/student/catkin_ws/src/3806Robotics/src/agentWorld.txt"
     resultsFile = "/home/student/catkin_ws/src/3806Robotics/src/results.txt"
-    server = CentralServer(1, worldFile)
+    server = CentralServer(1, testFile)
     server.agentsList[0].environment.WriteWorld(testFile)
     server.RunAgents(batches = 1, printResults=True)
     server.RunTest(worldFile, resultsFile)
