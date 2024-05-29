29bbcadc90065570c866a7876e26f5cede5d14ae
Joshua-S-Gilbert <joshuagilbert77@gmail.com>
2024-05-29 12:31:35 +1000

    changed file locations to local

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ import numpy as np
 
 
 def main():
-    worldFile = "/home/student/catkin_ws/src/3806Robotics/src/world.txt"
-    testFile = "/home/student/catkin_ws/src/3806Robotics/src/agentWorld.txt"
-    resultsFile = "/home/student/catkin_ws/src/3806Robotics/src/results.txt"
+    worldFile = "world.txt"
+    testFile = "agentWorld.txt"
+    resultsFile = "results.txt"
     server = CentralServer(1, testFile)
     server.agentsList[0].environment.WriteWorld(testFile)
     server.RunAgents(batches = 1, printResults=True)
